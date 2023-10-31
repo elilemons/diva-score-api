0ddb054eab79ce595b323a465a57982dce4bb6f8
Eli Lemons <me@elilemons.com>
2023-10-31 17:15:13 -0400

    Webpack aliasing

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,19 +1,45 @@
+import dotenv from 'dotenv'
 import path from 'path'
 
-import { payloadCloud } from '@payloadcms/plugin-cloud'
-import { mongooseAdapter } from '@payloadcms/db-mongodb'
 import { webpackBundler } from '@payloadcms/bundler-webpack'
+import { mongooseAdapter } from '@payloadcms/db-mongodb'
+import { payloadCloud } from '@payloadcms/plugin-cloud'
 import { slateEditor } from '@payloadcms/richtext-slate'
 import { buildConfig } from 'payload/config'
 
-import Users from './collections/Users'
 import Admins from './collections/Admins'
+import Users from './collections/Users'
+
+dotenv.config({
+  path: path.resolve(__dirname, '../.env'),
+})
+
+const mockModulePath = path.resolve(__dirname, './mocks/emptyModule.js')
 
 export default buildConfig({
   admin: {
     user: Admins.slug,
     bundler: webpackBundler(),
+    webpack: (config) => ({
+      ...config,
+      resolve: {
+        ...config.resolve,
+        alias: [
+          'handlebars',
+          'inline-css',
+          path.resolve(__dirname, './email/sendTemplatedEmail'),
+          path.resolve(__dirname, './email/generateEmailHTML'),
+        ].reduce(
+          (alias, aliasPath) => ({
+            ...alias,
+            [aliasPath]: mockModulePath,
+          }),
+          config.resolve.alias,
+        ),
+      },
+    }),
   },
+
   editor: slateEditor({}),
   collections: [Admins, Users],
   typescript: {
@@ -27,4 +53,6 @@ export default buildConfig({
   db: mongooseAdapter({
     url: process.env.DATABASE_URI,
   }),
+  cors: [process.env.PAYLOAD_PUBLIC_APP_URL, 'capacitor://localhost'].filter(Boolean),
+  csrf: [process.env.PAYLOAD_PUBLIC_APP_URL, 'capacitor://localhost'].filter(Boolean),
 })
