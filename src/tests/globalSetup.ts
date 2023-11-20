6cdb853d15bed61fc00ed89e04dc594d4513b054
Eli Lemons <me@elilemons.com>
2023-11-20 17:24:47 -0500

    Jest configuration

---
+++ src/tests/globalSetup.ts
@@ -0,0 +1,45 @@
+import express from 'express'
+import { resolve } from 'path'
+import payload from 'payload'
+import testCredentials from '../collections/Admins/tests/credentials'
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config({
+  path: resolve(__dirname, '../../.env'),
+})
+
+const app = express()
+
+const globalSetup = async () => {
+  await payload.init({
+    secret: process.env.PAYLOAD_SECRET,
+    // mongoURL: process.env.MONGO_URL,
+    express: app,
+  })
+
+  app.listen(process.env.PORT, async () => {
+    console.log(`Express is now listening for incoming connections on port ${process.env.PORT}.`)
+  })
+
+  const response = await fetch(
+    `${process.env.PAYLOAD_PUBLIC_SERVER_URL}/api/admins/first-register`,
+    {
+      body: JSON.stringify({
+        email: testCredentials.email,
+        password: testCredentials.password,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      method: 'post',
+    },
+  )
+
+  const data = await response.json()
+
+  if (!data.user || !data.user.token) {
+    throw new Error('Failed to register first user')
+  }
+}
+
+export default globalSetup
