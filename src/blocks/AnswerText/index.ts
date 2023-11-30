6425af36b4bd82ce4653d1149952a948935651fd
Eli Lemons <me@elilemons.com>
2023-11-30 17:24:17 -0500

    Added description

--- src/blocks/AnswerText/index.ts
+++ src/blocks/AnswerText/index.ts
@@ -4,21 +4,26 @@ import { blockFields } from '../../fields/blockFields'
 
 export const AnswerText: Block = {
   slug: 'answerTextBlock',
+  interfaceName: 'AnswerTextBlock',
   fields: [
     blockFields({
       name: 'answerTextFields',
       fields: [
         {
-          // Label shown to the user
           type: 'text',
           name: 'answerTextFieldLabel',
           label: 'Answer Text Field Label',
+          admin: {
+            description: 'This is the label that is shown to the user.',
+          },
         },
         {
-          // The value the user fills out
-          type: 'richText',
+          type: 'text',
           name: 'answerTextValue',
           label: 'Answer Value',
+          admin: {
+            description: 'This is the value the user enters.',
+          },
         },
       ],
     }),
