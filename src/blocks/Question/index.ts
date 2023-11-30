abda943ce87d715582f0ade3690931e7ff35fa83
Eli Lemons <me@elilemons.com>
2023-11-30 17:24:29 -0500

    Added some new fields

--- src/blocks/Question/index.ts
+++ src/blocks/Question/index.ts
@@ -1,11 +1,30 @@
 import { Block } from 'payload/types'
 import { blockFields } from '../../fields/blockFields'
 import { AnswerCheckbox } from '../AnswerCheckbox'
+import { AnswerRichText } from '../AnswerRichText'
 import { AnswerText } from '../AnswerText'
 
 export const Question: Block = {
   slug: 'questionBlock',
+  interfaceName: 'QuestionBlock',
   fields: [
+    {
+      name: 'questionFieldName',
+      type: 'text',
+      label: 'Question Field Name',
+      required: true,
+      admin: {
+        description:
+          'questionSetNameQuestionNumber - Number should match question order. Example: body1, body2. Used for matching the answers to the question.',
+      },
+    },
+    {
+      name: 'questionOrder',
+      type: 'number',
+      label: 'Question Order',
+      required: true,
+      min: 0,
+    },
     blockFields({
       name: 'questionTextFields',
       fields: [
@@ -13,11 +32,18 @@ export const Question: Block = {
           type: 'text',
           name: 'question',
           label: 'Question',
+          required: true,
+          admin: {
+            description: 'This is the question presented to the user.',
+          },
         },
         {
           type: 'blocks',
           name: 'answer',
-          blocks: [AnswerCheckbox, AnswerText],
+          required: true,
+          blocks: [AnswerCheckbox, AnswerText, AnswerRichText],
+          minRows: 1,
+          maxRows: 1,
         },
       ],
     }),
