773f01e092e1fc0c6c4a7377d15c64c1b10e9d59
Eli Lemons <me@elilemons.com>
2023-12-11 16:46:23 -0500

    Added delete survey helper

--- src/tests/helpers.ts
+++ src/tests/helpers.ts
@@ -1,6 +1,7 @@
 import { UserOnRequest } from '@elilemons/diva-score-lib'
 import { Admin } from 'payload/generated-types'
 import testCredentials from '../collections/Admins/tests/credentials'
+import Surveys from '../collections/Surveys'
 
 export const getAdmin = async (): Promise<UserOnRequest<Admin>> => {
   const adminLoginJSON: UserOnRequest<Admin> = await fetch(
@@ -18,4 +19,15 @@ export const getAdmin = async (): Promise<UserOnRequest<Admin>> => {
   ).then((res) => res.json())
 
   return adminLoginJSON
+}
+
+type DeleteSurveyProps = {
+  surveyId: string
+  headers: Headers
+}
+export const deleteSurvey = async ({ surveyId, headers }: DeleteSurveyProps) => {
+  return await fetch(`${process.env.PAYLOAD_PUBLIC_SERVER_URL}/api/${Surveys.slug}/${surveyId}`, {
+    method: 'delete',
+    headers,
+  }).then((res) => res.json())
 }
