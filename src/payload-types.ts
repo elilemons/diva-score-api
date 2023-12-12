64c196437c47c89a80af6a865f2fafcfd3492bfc
Eli Lemons <me@elilemons.com>
2023-12-12 16:47:20 -0500

    New fields on qs; answerRichText has proper props

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -62,6 +62,7 @@ export interface QuestionSet {
   active?: boolean | null;
   title?: string | null;
   pointValue?: number | null;
+  pointsEarned?: number | null;
   order?: number | null;
   questions?: QuestionBlock[] | null;
   updatedAt: string;
@@ -70,6 +71,7 @@ export interface QuestionSet {
 export interface QuestionBlock {
   questionFieldName: string;
   questionOrder: number;
+  requiredForSetPoint?: boolean | null;
   questionTextFields: {
     question: string;
     answer: (AnswerCheckboxBlock | AnswerTextBlock | AnswerRichTextBlock)[];
@@ -99,7 +101,7 @@ export interface AnswerTextBlock {
 export interface AnswerRichTextBlock {
   answerRichTextFields?: {
     answerRichTextFieldLabel?: string | null;
-    answerTextValue?:
+    answerRichTextValue?:
       | {
           [k: string]: unknown;
         }[]
