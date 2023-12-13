b36331cc886133e453243472953a05215dcb0b00
Eli Lemons <me@elilemons.com>
2023-12-13 17:29:44 -0500

    Added a new create survey test helper

--- src/tests/helpers.ts
+++ src/tests/helpers.ts
@@ -1,5 +1,8 @@
-import { Admin, UserOnRequest } from '@elilemons/diva-score-lib'
+import { Admin, Doc, UserOnRequest } from '@elilemons/diva-score-lib'
+import { Survey } from 'payload/generated-types'
 import testCredentials from '../collections/Admins/tests/credentials'
+import QuestionSets from '../collections/QuestionSets'
+import { mockQuestionSets } from '../collections/QuestionSets/tests/mock'
 import Surveys from '../collections/Surveys'
 
 export const getAdmin = async (): Promise<UserOnRequest<Admin>> => {
@@ -18,6 +21,27 @@ export const getAdmin = async (): Promise<UserOnRequest<Admin>> => {
   ).then((res) => res.json())
 
   return adminLoginJSON
+}
+
+type CreateSurveyProps = {
+  headers: Headers
+}
+export const createSurvey = async ({ headers }: CreateSurveyProps): Promise<Doc<Survey>> => {
+  mockQuestionSets.map(async (questionSet) => {
+    await fetch(`${process.env.PAYLOAD_PUBLIC_SERVER_URL}/api/${QuestionSets.slug}`, {
+      method: 'post',
+      headers,
+      body: JSON.stringify(questionSet),
+    })
+  })
+
+  return await fetch(`${process.env.PAYLOAD_PUBLIC_SERVER_URL}/api/${Surveys.slug}`, {
+    method: 'post',
+    headers,
+    body: JSON.stringify({
+      title: 'Test Survey',
+    }),
+  }).then((res) => res.json())
 }
 
 type DeleteSurveyProps = {
